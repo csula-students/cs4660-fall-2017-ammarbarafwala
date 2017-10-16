e093af7632742edace98416feaddf8565ad44a4f
Ammar <ammarbarafwala@gmail.com>
2017-10-15 22:36:18 -0700

    Quiz 2

--- cs4660/quiz/main.py
+++ cs4660/quiz/main.py
@@ -9,6 +9,7 @@ TODO: implement Dijkstra utilizing the path with highest effect number
 import json
 import time
 from collections import deque
+import heapq
 import codecs
 
 
@@ -122,18 +123,65 @@ def bfs(initial_id, dest_id):
     G_name[dest_id] = room_state['location']['name']
     return route_list(initial_id,dest_id,G,G_name)
 
-def dijkstra(initial_node, dest_node):
-    
-    pass
+def dijkstra(initial_id, dest_id):
+    queue = []
+    G = {}
+    S={}
+    # heapq.heapify(queue)
+    # heapq.heappush(queue, Data('',initial_id, None, 0))
+    queue.append(Data('',initial_id, None, 0))
+    while queue:
+        print(len(queue))
+        sorted(queue, key=lambda x: x.cost)
+        parent_node = queue.pop()#heapq.heappop(queue)
+        G[parent_node.id] = parent_node
+        room_state = get_state(parent_node.id)
+        parent_node.name=room_state['location']['name']
+        for each_node in room_state['neighbors']:
+            path_cost = parent_node.cost + transition_state(parent_node.id, each_node['id'])['event']['effect']
+            if each_node['id'] not in G and (each_node['id'] not in S or S[each_node['id']].cost<path_cost):
+                if each_node['id'] in S:
+                    queue.remove(S[each_node['id']])
+                data=Data('',each_node['id'], parent_node.id, path_cost)
+                queue.append(data)
+                S[each_node['id']]=data
+                # heapq.heappush(queue, data)
+
+    route_list = []
+    print("hi")
+    current_id = dest_id
+    current_node = G[dest_id]
+    while current_id != initial_id:
+        parent_node=G[current_id]
+        route_list.append(parent_node.name+'('+parent_node.id+'):'
+                          +current_node.name+'('+current_node.id+'):'
+                          +str(current_node.cost-parent_node.cost))
+        current_id = parent_node.parent_id
+        current_node=parent_node
+    route_list.reverse()
+    return route_list
+
+
+class Data:
+    def __init__(self, name, id, parent_id, cost):
+        self.name = name
+        self.id = id
+        self.cost = cost
+        self.parent_id = parent_id
+
+    def __eq__(self, other):
+        return self.cost == other.cost
+
+    def __lt__(self, other):
+        return self.cost < other.cost
 
 if __name__ == "__main__":
 
     # Your code starts here
     empty_room = get_state('7f3dc077574c013d98b2de8f735058b4')
     next_room = get_state("44dfaae131fa9d0a541c3eb790b57b00")
-    start_time = time.time()
-    path=bfs('7f3dc077574c013d98b2de8f735058b4','f1f131f647621a4be7c71292e79613f9')
+    path=dijkstra('7f3dc077574c013d98b2de8f735058b4','f1f131f647621a4be7c71292e79613f9')
     # print(empty_room)
+    # print(transition_state(empty_room['id'], empty_room['neighbors'][0]['id']))
     for each_value in path:
         print(each_value)
-    print("--- %s seconds ---" % (time.time() - start_time))
